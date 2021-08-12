7710132491a6adc942de7e414d5504d96e58b8d8
Mboya Michael <mikemboya2@gmail.com>
2021-08-12 09:29:21 +0300

    feat: handle auth exceptions

--- lib/authentication/repositories/authentication_repository.dart
+++ lib/authentication/repositories/authentication_repository.dart
@@ -5,3 +5,9 @@ abstract class AuthRepository {
   Future<void> loginWithEmailAndPassword(String email, String password);
   Stream<User?> getUser();
 }
+
+class EmailAlreadyInUseException implements Exception {}
+class EmailDoesNotExistException implements Exception {}
+class InvalidEmailException implements Exception {}
+class WeakPasswordException implements Exception {}
+class WrongPasswordException implements Exception {}
